8056729abbfb851d4f88c0c8932756bdc2145f2d
jagsbit <114154412+jagsbit@users.noreply.github.com>
2023-12-21 20:13:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,29 @@
 //paper-2
 //scissor-3
 document.querySelector(".play").addEventListener("click",function(){
-    var a=Math.floor(3*Math.random())+1;
+    var a=prompt("Enter your choice stone-1 paper -2 scissor-3");
     var b=Math.floor(3*Math.random())+1;
     var winner=0;
     document.querySelector(".img1").setAttribute("src","img"+a+".png");
     document.querySelector(".img2").setAttribute("src","img"+b+".png");
      
-    if((a===1&&b===2)||(a===2&&b===1)){
+    if((a==1&&b==2)||(a==2&&b==1)){
         winner=2;
     }
-    else if((a===2&&b===3)||(a===3&&b===2)){
+    else if((a==2&&b==3)||(a==3&&b==2)){
         winner=3;
     }
-    else if((a===1&&b===3)||(a===3&&b===1)){
+    else if((a==1&&b==3)||(a==3&&b==1)){
         winner=1;
     }
-    if(winner===0){
+    if(winner==0){
         document.querySelector("h1").innerHTML="draw";
     }
-    else if(winner===a){
-        document.querySelector("h1").innerHTML="Player 1 wins";
+    else if(winner==a){
+        document.querySelector("h1").innerHTML="You Won";
     }
     else{
-        document.querySelector("h1").innerHTML="Player 2 wins";
+        document.querySelector("h1").innerHTML="Jags won";
     }
 });
 
